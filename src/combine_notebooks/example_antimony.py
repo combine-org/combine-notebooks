7062ae4aa05b032f49b79b02db942ef53b3cdba2
Matthias Koenig <konigmatt@googlemail.com>
2022-06-28 17:07:46 +0200

    work on antimony example

--- src/combine_notebooks/example_antimony.py
+++ src/combine_notebooks/example_antimony.py
@@ -1,16 +1,22 @@
 """Create repressilator with antimony.
 
 http://antimony.sourceforge.net/
+https://tellurium.readthedocs.io/en/latest/antimony.html
 """
 from pathlib import Path
+
+import antimony
+
+from combine_notebooks.validation_sbml import validate_sbml
 
 
 def create_repressilator(sbml_path: Path) -> None:
     """Create repressilator with antimony."""
     model: str = '''
-        model antimony_repressilator:
-            #compartment
+        model antimony_repressilator
+            # compartment
             compartment cell = 1
+            
             # species
             species PX = 0
             species PY = 0
@@ -18,6 +24,7 @@ def create_repressilator(sbml_path: Path) -> None:
             species X = 0
             species Y = 20
             species Z = 0
+            
             # parameters
             beta = 0.2
             alpha0 = 0.2164
@@ -29,32 +36,45 @@ def create_repressilator(sbml_path: Path) -> None:
             tau_prot = 10
             ps_a = 0.5
             ps_0 = 0.0005
+            
             # assignment rules
-            t_ave = tau_mRNA/ln(2)
-            beta = tau_mRNA/tau_prot
-            k_tl = eff/t_ave
-            a_tr = (ps_a - ps_0)*60
-            a0_tr = ps_0*60
-            kd_prot = ln(2)/tau_prot
-            kd_mRNA = ln(2)/tau_mRNA
-            alpha = (a_tr*eff*tau_prot)/(ln(2)*KM)
-            alpha0 = (a0_tr*eff*tau_prot)/(ln(2)*KM)
-            # reactions
-            X ->
-            Y ->
-            Z ->
-            -> PX
-            -> PY
-            -> PZ
-            PX ->
-            PY ->
-            PZ ->
-            -> X
-            -> Y
-            -> Z
+            # t_ave := tau_mRNA/ln(2)
+            # beta := tau_mRNA/tau_prot
+            # k_tl = eff/t_ave
+            # a_tr = (ps_a - ps_0)*60
+            # a0_tr = ps_0*60
+            # kd_prot = ln(2)/tau_prot
+            # kd_mRNA = ln(2)/tau_mRNA
+            # alpha = (a_tr*eff*tau_prot)/(ln(2)*KM)
+            # alpha0 = (a0_tr*eff*tau_prot)/(ln(2)*KM)
+            # 
+            # # reactions
+            # X ->
+            # Y ->
+            # Z ->
+            # -> PX
+            # -> PY
+            # -> PZ
+            # PX ->
+            # PY ->
+            # PZ ->
+            # -> X
+            # -> Y
+            # -> Z
         end
     '''
     print(model)
+
+    status = antimony.loadAntimonyString(model)
+    print(f"Antimony status: {status}")
+    print(antimony.getLastError())
+    sbml_str = antimony.getSBMLString()
+    print(sbml_str)
+    print(antimony.getSBMLWarnings())
+
+    import libsbml
+    doc = libsbml.readSBMLFromString(sbml_str)
+    validate_sbml(doc)
 
 
 if __name__ == "__main__":
--- src/combine_notebooks/example_libsbml.py
+++ src/combine_notebooks/example_libsbml.py
@@ -4,9 +4,10 @@ Create the repressilator step by step.
 See on biomodels.
 """
 from pathlib import Path
-from typing import List
 
 import libsbml
+
+from combine_notebooks.validation_sbml import validate_sbml
 
 
 def create_repressilator(sbml_path: Path) -> libsbml.SBMLDocument:
@@ -410,50 +411,6 @@ def create_repressilator(sbml_path: Path) -> libsbml.SBMLDocument:
     validate_sbml(doc, units_consistency=False)
 
     return doc
-
-
-def validate_sbml(doc: libsbml.SBMLDocument, units_consistency: bool = False) -> None:
-    """Validate sbml."""
-    # set the unit checking, similar for the other settings
-    doc.setConsistencyChecks(libsbml.LIBSBML_CAT_UNITS_CONSISTENCY, units_consistency)
-    doc.checkConsistency()
-
-    # get errors/warnings
-    n_errors: int = doc.getNumErrors()
-    errors: List[libsbml.SBMLError] = list()
-    warnings: List[libsbml.SBMLError] = list()
-    for k in range(n_errors):
-        error: libsbml.SBMLError = doc.getError(k)
-        severity = error.getSeverity()
-        if (severity == libsbml.LIBSBML_SEV_ERROR) or (
-            severity == libsbml.LIBSBML_SEV_FATAL
-        ):
-            errors.append(error)
-        else:
-            warnings.append(error)
-
-    # print results
-    print("-" * 80)
-    print(f"{'validation error(s)':<25}: {len(errors)}")
-    print(f"{'validation warning(s)':<25}: {len(warnings)}")
-
-    if len(errors) > 0:
-        print("--- errors ---")
-        for kerr in enumerate(errors):
-            print(f"E{kerr}: {error.getCategoryAsString()} L{error.getLine()}")
-            print(
-                f"[{error.getSeverityAsString()}] {error.getShortMessage()} | {error.getMessage()}"
-            )
-
-    if len(warnings) > 0:
-        print("--- warnings ---")
-        for kwarn in enumerate(warnings):
-            print(f"E{kwarn}: {error.getCategoryAsString()} L{error.getLine()}")
-            print(
-                f"[{error.getSeverityAsString()}] {error.getShortMessage()} | {error.getMessage()}"
-            )
-
-    print("-" * 80)
 
 
 if __name__ == "__main__":
