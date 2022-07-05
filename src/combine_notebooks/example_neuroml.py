52d8468585a4864428418cb7546ae4357dfba407
aditya-ml <aditya98singhal@gmail.com>
2022-07-05 19:34:47 +0530

    neuroml

--- src/combine_notebooks/example_neuroml.py
+++ src/combine_notebooks/example_neuroml.py
@@ -1,1 +1,12 @@
 """Example script for creating SBML repressilator with neuroml."""
+from pathlib import Path
+import pyneuroml
+from neuroml import NeuroMLDocument
+
+nml_doc = NeuroMLDocument(id="repressilator")
+
+#compartment
+#species
+#parameters
+#rules
+#reactions
