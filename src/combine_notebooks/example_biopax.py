cfb17e81d510ed273062e454693487e4693749cf
Matthias Koenig <konigmatt@googlemail.com>
2022-08-16 16:09:12 +0200

    biopax documentation

---
+++ src/combine_notebooks/example_biopax.py
@@ -0,0 +1,7 @@
+"""
+http://www.biopax.org/
+
+Biological Pathway Exchange (BioPAX) is a standard language that aims to enable integration, exchange, visualization and analysis of biological pathway data. Specifically, BioPAX supports data exchange between pathway data groups and thus reduces the complexity of interchange between data formats by providing an accepted standard format for pathway data. It is an open and collaborative effort by the community of researchers, software developers, and institutions. BioPAX is defined in OWL DL and is represented in the RDF/XML format. For more details, see Demir E et al. 2010. The BioPAX community standard for pathway data sharing, Nature Biotechnology. 28(9).
+
+Specification: http://www.biopax.org/release/biopax-level3-documentation.pdf
+"""
