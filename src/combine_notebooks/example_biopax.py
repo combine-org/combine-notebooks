c7eaf16fee6924aa5577d142ea637f8349053497
aditya-ml <aditya98singhal@gmail.com>
2022-08-21 09:00:39 -0400

    modified biopax

--- src/combine_notebooks/example_biopax.py
+++ src/combine_notebooks/example_biopax.py
@@ -13,7 +13,8 @@ https://github.com/indralab/pybiopax/blob/master/notebooks/tutorial.ipynb
 """
 
 import pybiopax
-from combine_notebooks import RESOURCES_DIR
+# from combine_notebooks import RESOURCES_DIR
+from pybiopax.biopax import BiochemicalReaction, Stoichiometry, Provenance, PublicationXref
 
 from pybiopax.biopax.model import BioPaxModel
 from pybiopax.biopax.base import Protein
@@ -38,9 +39,61 @@ Protein(LacI protein)
 '_member_physical_entity_of': {PhysicalEntity(PX), PhysicalEntity(PX), PhysicalEntity(PX)}, 'entity_reference': None}
 
 '''
-p = Protein(uid="PX", display_name="LacI protein")
-objects.append(p)
+p1 = Protein(uid="PX", display_name="LacI protein")
+objects.append(p1)
 
+'''
+<bp:PhysicalEntity rdf:about="RIGHT_0_conversion_Reaction5_PY">
+ <bp:cellularLocation rdf:resource="cell" />
+ <bp:displayName rdf:datatype = "http://www.w3.org/2001/XMLSchema#string">PY</bp:displayName>
+ <bp:memberPhysicalEntity rdf:resource="PY" />
+</bp:PhysicalEntity>
+
+'''
+p2 = Protein(uid="RIGHT_0_conversion_Reaction5_PY", displayName="PY")
+objects.append(p2)
+
+'''
+<bp:Stoichiometry rdf:about="LEFT_0_conversion_Reaction2_Y_STOICHIOMETRY">
+ <bp:physicalEntity rdf:resource="LEFT_0_conversion_Reaction2_Y" />
+ <bp:stoichiometricCoefficient rdf:datatype = "http://www.w3.org/2001/XMLSchema#float">1.0</bp:stoichiometricCoefficient>
+</bp:Stoichiometry>
+'''
+s1 = Stoichiometry(uid="LEFT_0_conversion_Reaction2_Y_STOICHIOMETRY", stoichiometricCoefficient="1")
+objects.append(s1)
+
+'''
+<bp:Provenance rdf:about="datasource_1">
+ <bp:xref rdf:resource="http://identifiers.org/biomodels.db/BIOMD0000000012" />
+ <bp:displayName rdf:datatype = "http://www.w3.org/2001/XMLSchema#string">BioModels Database</bp:displayName>
+</bp:Provenance>
+'''
+p3 = Provenance(uid="datasource_1", displayName="BioModels Database")
+objects.append(p3)
+
+p4 = PublicationXref(uid="10659856", displayName="PubMed")
+objects.append(p4)
+
+'''
+<bp:Control rdf:about="control_Reaction4_X_0">
+ <bp:controlled rdf:resource="conversion_Reaction4" />
+ <bp:controlType rdf:datatype = "http://www.w3.org/2001/XMLSchema#string">INHIBITION</bp:controlType>
+ <bp:controller rdf:resource="PEP0_control_Reaction4X" />
+</bp:Control>
+'''
+c1 = Control(uid="control_Reaction4_X_0", controlType="INHIBITION")
+objects.append(c1)
+
+'''
+<bp:BiochemicalReaction rdf:about="conversion_Reaction12">
+ <bp:xref rdf:resource="http://identifiers.org/obo.go/GO:0006351" />
+ <bp:participantStoichiometry rdf:resource="RIGHT_0_conversion_Reaction12_Z_STOICHIOMETRY" />
+ <bp:displayName rdf:datatype = "http://www.w3.org/2001/XMLSchema#string">transcription of CI</bp:displayName>
+ <bp:right rdf:resource="RIGHT_0_conversion_Reaction12_Z" />
+</bp:BiochemicalReaction>
+'''
+b1=BiochemicalReaction(uid="conversion_Reaction12", displayName="transcription of CI")
+objects.append(b1)
 
 model = BioPaxModel(objects={o.uid: o for o in objects}, xml_base="http://www.biopax.org/release/biopax-level3.owl#")
 owl_str: str = model_to_owl_str(model)
