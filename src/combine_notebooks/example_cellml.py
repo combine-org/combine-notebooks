aaa07de3c107bc6210a170c173d86c5e18103379
aditya-ml <aditya98singhal@gmail.com>
2022-08-25 03:06:16 -0400

    modified cellml

--- src/combine_notebooks/example_cellml.py
+++ src/combine_notebooks/example_cellml.py
@@ -38,7 +38,7 @@ def create_model(cellml_path: Path) -> libcellml.Model:
     # first_order_rate_constant = libcellml.Units("minute")
     # first_order_rate_constant.addUnit("minute")
     # model.addUnits(first_order_rate_constant)
-
+    ##############################################################################################
     '''
     <component name="environment">
       <variable units="minute" public_interface="out" name="time" cmeta:id="environment_time"/>
@@ -50,8 +50,9 @@ def create_model(cellml_path: Path) -> libcellml.Model:
 
     var_n = libcellml.Variable()
     var_n.setName("time")
-    # var_n.setInterfaceType("out")
+    var_n.setInterfaceType("out")
     var_n.setUnits("minute")
+    var_n.setId("environment_time")
     comp_env.addVariable(var_n)
 
     comp_parameters = libcellml.Component()
@@ -114,26 +115,516 @@ def create_model(cellml_path: Path) -> libcellml.Model:
     # var_n.setInterfaceType()
     comp_parameters.addVariable(var_t_ave)
 
-    var_eff = libcellml.Variable()
-    var_eff.setName("eff")
-    var_eff.setInitialValue(20)
-    var_eff.setUnits("minute")
-    comp_parameters.addVariable(var_eff)
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
 
     # start with math
-    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("eff / t_ave")
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("mRNA_halflife/ln(2)")
     math_str = libsbml.writeMathMLToString(math_ast1)
     print(math_str)
-
     math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
-
-    #  1.g
     #    Include the MathML strings in the component.
     math_str = "\n".join([math_header] + math_str.split("\n")[2:])
     print(math_str)
-
     comp_parameters.setMath(math_str)
     
+    ##############################################################################################
+
+    comp_parameters = libcellml.Component()
+    comp_parameters.setName("M_lacl")
+    model.addComponent(comp_parameters)
+
+    # var n: dimensionless {init: 2, pub: out};
+    var_n = libcellml.Variable()
+    var_n.setName("M_lacl")
+    var_n.setInitialValue(5.0)
+    var_n.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_n)
+
+    
+
+    var_alpha = libcellml.Variable()
+    var_alpha.setName("alpha")
+    var_alpha.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("P_cl")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("n")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_alpha_0 = libcellml.Variable()
+    var_alpha_0.setName("alpha_0")
+    var_alpha_0.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha_0)
+
+    
+
+    var_K_m = libcellml.Variable()
+    var_K_m.setName("K_m")
+    # var_K_m.setInitialValue(40)
+    var_K_m.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_K_m)
+
+    var_efficiency = libcellml.Variable()
+    var_efficiency.setName("efficiency")
+    # var_efficiency.setInitialValue(20)
+    var_efficiency.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_efficiency)
+
+    
+
+    var_t_ave = libcellml.Variable()
+    var_t_ave.setName("t_ave")
+    var_t_ave.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_t_ave)
+
+    var_mRNA_halflife = libcellml.Variable()
+    var_mRNA_halflife.setName("time")
+    # var_mRNA_halflife.setInitialValue(2)
+    var_mRNA_halflife.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_mRNA_halflife)
+
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
+
+    # start with math
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("a0_tr + (a_tr * power(KM, n)) / (power(KM, n) + power(PZ, n))")
+    math_str = libsbml.writeMathMLToString(math_ast1)
+    print(math_str)
+    math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
+    #    Include the MathML strings in the component.
+    math_str = "\n".join([math_header] + math_str.split("\n")[2:])
+    print(math_str)
+    comp_parameters.setMath(math_str)
+
+    ##############################################################################################
+
+    comp_parameters = libcellml.Component()
+    comp_parameters.setName("M_tetR")
+    model.addComponent(comp_parameters)
+
+    # var n: dimensionless {init: 2, pub: out};
+    var_n = libcellml.Variable()
+    var_n.setName("M_tetR")
+    var_n.setInitialValue(0.0)
+    var_n.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_n)
+
+    
+
+    var_alpha = libcellml.Variable()
+    var_alpha.setName("alpha")
+    var_alpha.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("P_lacl")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("n")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_alpha_0 = libcellml.Variable()
+    var_alpha_0.setName("alpha_0")
+    var_alpha_0.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha_0)
+
+    
+
+    var_K_m = libcellml.Variable()
+    var_K_m.setName("K_m")
+    # var_K_m.setInitialValue(40)
+    var_K_m.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_K_m)
+
+    var_efficiency = libcellml.Variable()
+    var_efficiency.setName("efficiency")
+    # var_efficiency.setInitialValue(20)
+    var_efficiency.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_efficiency)
+
+    
+
+    var_t_ave = libcellml.Variable()
+    var_t_ave.setName("t_ave")
+    var_t_ave.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_t_ave)
+
+    var_mRNA_halflife = libcellml.Variable()
+    var_mRNA_halflife.setName("time")
+    # var_mRNA_halflife.setInitialValue(2)
+    var_mRNA_halflife.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_mRNA_halflife)
+
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
+
+    # start with math
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("a0_tr + (a_tr * power(KM, n)) / (power(KM, n) + power(PZ, n))")
+    math_str = libsbml.writeMathMLToString(math_ast1)
+    print(math_str)
+    math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
+    #    Include the MathML strings in the component.
+    math_str = "\n".join([math_header] + math_str.split("\n")[2:])
+    print(math_str)
+    comp_parameters.setMath(math_str)
+
+    ##############################################################################################
+
+    comp_parameters = libcellml.Component()
+    comp_parameters.setName("M_cl")
+    model.addComponent(comp_parameters)
+
+    # var n: dimensionless {init: 2, pub: out};
+    var_n = libcellml.Variable()
+    var_n.setName("M_cl")
+    var_n.setInitialValue(15.0)
+    var_n.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_n)
+
+    
+
+    var_alpha = libcellml.Variable()
+    var_alpha.setName("alpha")
+    var_alpha.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("P_tetR")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("n")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_alpha_0 = libcellml.Variable()
+    var_alpha_0.setName("alpha_0")
+    var_alpha_0.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha_0)
+
+    
+
+    var_K_m = libcellml.Variable()
+    var_K_m.setName("K_m")
+    # var_K_m.setInitialValue(40)
+    var_K_m.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_K_m)
+
+    var_efficiency = libcellml.Variable()
+    var_efficiency.setName("efficiency")
+    # var_efficiency.setInitialValue(20)
+    var_efficiency.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_efficiency)
+
+    
+
+    var_t_ave = libcellml.Variable()
+    var_t_ave.setName("t_ave")
+    var_t_ave.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_t_ave)
+
+    var_mRNA_halflife = libcellml.Variable()
+    var_mRNA_halflife.setName("time")
+    # var_mRNA_halflife.setInitialValue(2)
+    var_mRNA_halflife.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_mRNA_halflife)
+
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
+
+    # start with math
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("a0_tr + (a_tr * power(KM, n)) / (power(KM, n) + power(PZ, n))")
+    math_str = libsbml.writeMathMLToString(math_ast1)
+    print(math_str)
+    math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
+    #    Include the MathML strings in the component.
+    math_str = "\n".join([math_header] + math_str.split("\n")[2:])
+    print(math_str)
+    comp_parameters.setMath(math_str)
+
+##############################################################################################
+
+    comp_parameters = libcellml.Component()
+    comp_parameters.setName("P_lacl")
+    model.addComponent(comp_parameters)
+
+    # var n: dimensionless {init: 2, pub: out};
+    var_n = libcellml.Variable()
+    var_n.setName("P_lacl")
+    var_n.setInitialValue(0.0)
+    var_n.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_n)
+
+    
+
+    var_alpha = libcellml.Variable()
+    var_alpha.setName("beta")
+    var_alpha.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("M_lacl")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_t_ave = libcellml.Variable()
+    var_t_ave.setName("t_ave")
+    var_t_ave.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_t_ave)
+    
+
+    var_K_m = libcellml.Variable()
+    var_K_m.setName("K_m")
+    # var_K_m.setInitialValue(40)
+    var_K_m.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_K_m)
+
+    var_efficiency = libcellml.Variable()
+    var_efficiency.setName("efficiency")
+    # var_efficiency.setInitialValue(20)
+    var_efficiency.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_efficiency)
+
+
+    var_mRNA_halflife = libcellml.Variable()
+    var_mRNA_halflife.setName("time")
+    # var_mRNA_halflife.setInitialValue(2)
+    var_mRNA_halflife.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_mRNA_halflife)
+
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
+
+    # start with math
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("a0_tr + (a_tr * power(KM, n)) / (power(KM, n) + power(PZ, n))")
+    math_str = libsbml.writeMathMLToString(math_ast1)
+    print(math_str)
+    math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
+    #    Include the MathML strings in the component.
+    math_str = "\n".join([math_header] + math_str.split("\n")[2:])
+    print(math_str)
+    comp_parameters.setMath(math_str)
+
+##############################################################################################
+
+    comp_parameters = libcellml.Component()
+    comp_parameters.setName("P_tetR")
+    model.addComponent(comp_parameters)
+
+    # var n: dimensionless {init: 2, pub: out};
+    var_n = libcellml.Variable()
+    var_n.setName("P_tetR")
+    var_n.setInitialValue(0.0)
+    var_n.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_n)
+
+    
+
+    var_alpha = libcellml.Variable()
+    var_alpha.setName("beta")
+    var_alpha.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("M_tetR")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_t_ave = libcellml.Variable()
+    var_t_ave.setName("t_ave")
+    var_t_ave.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_t_ave)
+    
+
+    var_K_m = libcellml.Variable()
+    var_K_m.setName("K_m")
+    # var_K_m.setInitialValue(40)
+    var_K_m.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_K_m)
+
+    var_efficiency = libcellml.Variable()
+    var_efficiency.setName("efficiency")
+    # var_efficiency.setInitialValue(20)
+    var_efficiency.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_efficiency)
+
+
+    var_mRNA_halflife = libcellml.Variable()
+    var_mRNA_halflife.setName("time")
+    # var_mRNA_halflife.setInitialValue(2)
+    var_mRNA_halflife.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_mRNA_halflife)
+
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
+
+    # start with math
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("a0_tr + (a_tr * power(KM, n)) / (power(KM, n) + power(PZ, n))")
+    math_str = libsbml.writeMathMLToString(math_ast1)
+    print(math_str)
+    math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
+    #    Include the MathML strings in the component.
+    math_str = "\n".join([math_header] + math_str.split("\n")[2:])
+    print(math_str)
+    comp_parameters.setMath(math_str)
+
+##############################################################################################
+
+    comp_parameters = libcellml.Component()
+    comp_parameters.setName("P_cl")
+    model.addComponent(comp_parameters)
+
+    # var n: dimensionless {init: 2, pub: out};
+    var_n = libcellml.Variable()
+    var_n.setName("P_cl")
+    var_n.setInitialValue(0.0)
+    var_n.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_n)
+
+    
+
+    var_alpha = libcellml.Variable()
+    var_alpha.setName("beta")
+    var_alpha.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_alpha)
+
+    var_beta = libcellml.Variable()
+    var_beta.setName("M_cl")
+    # var_beta.setInitialValue(0.2)
+    var_beta.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_beta)
+
+    var_t_ave = libcellml.Variable()
+    var_t_ave.setName("t_ave")
+    var_t_ave.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_t_ave)
+    
+
+    var_K_m = libcellml.Variable()
+    var_K_m.setName("K_m")
+    # var_K_m.setInitialValue(40)
+    var_K_m.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_K_m)
+
+    var_efficiency = libcellml.Variable()
+    var_efficiency.setName("efficiency")
+    # var_efficiency.setInitialValue(20)
+    var_efficiency.setUnits("dimensionless")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_efficiency)
+
+
+    var_mRNA_halflife = libcellml.Variable()
+    var_mRNA_halflife.setName("time")
+    # var_mRNA_halflife.setInitialValue(2)
+    var_mRNA_halflife.setUnits("minute")
+    # var_n.setInterfaceType()
+    comp_parameters.addVariable(var_mRNA_halflife)
+
+    # var_eff = libcellml.Variable()
+    # var_eff.setName("eff")
+    # var_eff.setInitialValue(20)
+    # var_eff.setUnits("minute")
+    # comp_parameters.addVariable(var_eff)
+
+    # start with math
+    math_ast1: libsbml.ASTNode = libsbml.parseL3Formula("a0_tr + (a_tr * power(KM, n)) / (power(KM, n) + power(PZ, n))")
+    math_str = libsbml.writeMathMLToString(math_ast1)
+    print(math_str)
+    math_header = '<math xmlns="http://www.w3.org/1998/Math/MathML" xmlns:cellml="http://www.cellml.org/cellml/2.0#">'
+    #    Include the MathML strings in the component.
+    math_str = "\n".join([math_header] + math_str.split("\n")[2:])
+    print(math_str)
+    comp_parameters.setMath(math_str)
+############################################################
+
+    comp_env = libcellml.Connection()
+    
+    model.addComponent(comp_env)
+
     #  Checking that it worked
     print("-" * 80)
     print_model(model)
