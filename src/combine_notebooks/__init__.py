c412981d519ad09b664e60d9dbefd52664501bd4
Matthias Koenig <konigmatt@googlemail.com>
2022-09-10 21:16:45 +0200

    Bump version: 0.1.0 → 0.1.1

--- src/combine_notebooks/__init__.py
+++ src/combine_notebooks/__init__.py
@@ -1,7 +1,7 @@
 """combine_notebooks - COMBINE jupyter notebooks in python."""
 from pathlib import Path
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 BASE_DIR: Path = Path(__file__).parent.parent.parent
 RESULTS_DIR: Path = BASE_DIR / "results"
