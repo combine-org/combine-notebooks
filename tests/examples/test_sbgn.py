759cfb402af8d9248852a194ddf0137fce94ad2d
pgleeson <p.gleeson@gmail.com>
2023-05-24 10:58:36 +0100

    Disabling tests failing due to issue with sysbioapps.spdns.org
    See https://github.com/combine-org/combine-notebooks/issues/26

--- tests/examples/test_sbgn.py
+++ tests/examples/test_sbgn.py
@@ -1,9 +1,14 @@
 """Test SBGN functionality."""
 from pathlib import Path
 
+import pytest
+
 from combine_notebooks.examples import example_sbgn
 
 
+@pytest.mark.skip(
+    reason="Failure to communicate with sysbioapps.spdns.org; see https://github.com/combine-org/combine-notebooks/issues/26"
+)
 def test_create_repressilator_libsbgn(tmp_path: Path) -> None:
     """Test creation of repressilator SBGN with libsbgn."""
     sbgn_path: Path = tmp_path / "test.xml"
