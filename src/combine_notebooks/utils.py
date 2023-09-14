8892374434e0720e9e4bddd0a615d1339452ca56
pgleeson <p.gleeson@gmail.com>
2023-09-14 12:09:34 +0100

    Add helper method for determining environment

---
+++ src/combine_notebooks/utils.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import os
+
+def determine_environment():
+
+    #determine if we're running on Google Colab
+    try:
+        import google.colab
+        exec_env = "colab"      #we seem to be on colab
+        print("Assuming this notebook is running on Google Colab")
+    except:
+        exec_env = "binder"     #assume it's binder
+
+    if exec_env == "colab":
+        working_dir = f"{Path.cwd()}/combine-notebooks/notebooks/results"
+        os.system('git clone https://github.com/combine-org/combine-notebooks')
+        os.chdir('combine-notebooks')
+        os.system('pip install .')
+
+    else:
+        #binder starts off in the notebook's folder
+        working_dir = f"{Path.cwd()}/results"
+
+    os.chdir(working_dir)
+
+    print("Current directory is: %s"%working_dir)
+    return working_dir
